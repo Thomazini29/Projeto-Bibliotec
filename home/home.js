6a84898cb925564e77cf2e725acff5feb4e06368
MATHEUS THOMAZINI DE ANDRADE <54394278805@info.local>
2026-09-11 09:20:29 -0300

    :iphone: Adicionado responsividade e JavaScript

---
+++ home/home.js
@@ -0,0 +1,18 @@
+
+const menu = document.getElementById("menu"); // pegando o elemento pelo ID
+
+// estamos pegando o elemento de forma genérica - pode ser classe (precisa trazer o ponto) ou pode ser ID (precisa trazer o hashtag).
+const navMenu = document.querySelector(".navegacao")
+
+menu.addEventListener("click", function() {
+
+    if(navMenu.className == "navegacao") {
+        navMenu.className = "navegacao ativo";
+    }
+    else {
+        navMenu.className = "navegacao";
+    }
+}) 
+
+// Opção com arrow function -> função lambda
+// menu.addEventListener ("click", () => {}) 
